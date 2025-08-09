32a7b30ad093fcbc27372f25bbf68d8d58d6dea1
Rafi Siddique <rafisiddique67@gmail.com>
2025-08-09 15:21:00 +0600

    popular course section updated

--- src/Pages/Home/Classes/Classes.jsx
+++ src/Pages/Home/Classes/Classes.jsx
@@ -1,18 +1,19 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import Card from "../../../shared/Card/Card";
-import { useEffect, useState } from "react";
+import "swiper/css/pagination";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const Classes = () => {
-  const [Class, setClass] = useState([]);
-  const axiosPublic = useAxiosPublic();
+  const { get } = useAxiosPublic();
 
-  useEffect(() => {
-    axiosPublic.get("/classes").then((res) => setClass(res.data));
-  }, [axiosPublic]);
+  const { data: Class = [] } = useQuery({
+    queryKey: ["classes"],
+    queryFn: async () => await get("/classes").then((res) => res.data),
+  });
 
   return (
     <div className="mt-10 space-y-7">
@@ -20,15 +21,23 @@ const Classes = () => {
       <h1 className="text-4xl font-bold">Popular Courses</h1>
       {/* card section */}
       <Swiper
+        modules={[Pagination, Navigation, Autoplay]}
         navigation={true}
-        modules={[Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={true}
+        style={{
+          "--swiper-navigation-color": "#FDC800",
+          "--swiper-pagination-color": "#FDC800",
+        }}
         slidesPerView={3}
-        data-swiper-autoplay="2000"
+        loop={true}
         className="mySwiper max-w-screen-lg"
       >
-        {Class.map( data => (
+        {Class.map((data) => (
           <SwiperSlide>
-            <Card key={data._id} data={data}/>
+            <Card key={data._id} data={data} />
           </SwiperSlide>
         ))}
       </Swiper>
