b1e59cf2372135e2addbb884fd1916078bb1f504
Rafi Siddique <rafisiddique67@gmail.com>
2025-08-21 08:01:44 +0600

    dynamic title added'

--- src/Pages/Class/Class.jsx
+++ src/Pages/Class/Class.jsx
@@ -13,6 +13,7 @@ const Class = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+      <title>All Classes</title>
       {Classes.map((cl) => (
         <ClassCard key={cl._id} data={cl} />
       ))}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,24 @@
-import Banner from './Banner/Banner';
-import BecomeAnInstructor from './BecomeAnInstructor/BecomeAnInstructor';
-import CampusVideo from './CampusVideo/CampusVideo';
-import Classes from './Classes/Classes';
-import Feedback from './Feedback/Feedback';
-import Partner from './Partner/Partner';
-import SkilledInstructor from './SkilledInstructor/SkilledInstructor';
-import Stats from './Stats/Stats';
+import Banner from "./Banner/Banner";
+import BecomeAnInstructor from "./BecomeAnInstructor/BecomeAnInstructor";
+import CampusVideo from "./CampusVideo/CampusVideo";
+import Classes from "./Classes/Classes";
+import Feedback from "./Feedback/Feedback";
+import Partner from "./Partner/Partner";
+import SkilledInstructor from "./SkilledInstructor/SkilledInstructor";
+import Stats from "./Stats/Stats";
 
 const Home = () => {
   return (
     <div>
-      <Banner/>
-      <Partner/>
-      <Classes/>
-      <Feedback/>
-      <Stats/>
-      <BecomeAnInstructor/>
-      <CampusVideo/>
-      <SkilledInstructor/>
+      <title>Home</title>
+      <Banner />
+      <Partner />
+      <Classes />
+      <Feedback />
+      <Stats />
+      <BecomeAnInstructor />
+      <CampusVideo />
+      <SkilledInstructor />
     </div>
   );
 };
