0db38fd19047e0c3f061c6b73d3303496a1519ed
Rafi Siddique <rafisiddique67@gmail.com>
2025-08-30 20:44:55 +0600

    admin route added

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from "react-router";
+import useVerifyUser from "../hooks/useVerifyUser";
+import useAuth from "../hooks/useAuth";
+import Loading from "../shared/Loading/Loading";
+
+const AdminRoute = ({ children }) => {
+  const { verifyUser, isLoading } = useVerifyUser();
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (isLoading) return <Loading />;
+
+  if (user && verifyUser[0].role === "admin") return children;
+
+  return <Navigate to="/" state={{ from: location }} />;
+};
+
+export default AdminRoute;
