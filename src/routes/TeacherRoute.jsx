98fb933d797352871095d499db0cc6eb53e41495
Rafi Siddique <rafisiddique67@gmail.com>
2025-08-30 20:41:59 +0600

    teacher route added

--- src/routes/TeacherRoute.jsx
+++ src/routes/TeacherRoute.jsx
@@ -3,16 +3,16 @@ import useVerifyUser from "../hooks/useVerifyUser";
 import useAuth from "../hooks/useAuth";
 import Loading from "../shared/Loading/Loading";
 
-const StudentRoute = ({ children }) => {
+const TeacherRoute = ({ children }) => {
   const { verifyUser, isLoading } = useVerifyUser();
-  const {user} = useAuth();
+  const { user } = useAuth();
   const location = useLocation();
 
-  if(isLoading) return <Loading/>
+  if (isLoading) return <Loading />;
 
-  if (user && verifyUser[0].role === "student") return children;
+  if (user && verifyUser[0].role === "teacher") return children;
 
-  return <Navigate to="/" state={{from: location}}/>;
+  return <Navigate to="/" state={{ from: location }} />;
 };
 
-export default StudentRoute;
+export default TeacherRoute;
