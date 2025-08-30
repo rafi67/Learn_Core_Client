e4d06359a5d44d14a9f3b838d70650755097d1b1
Rafi Siddique <rafisiddique67@gmail.com>
2025-08-30 19:17:51 +0600

    class details page updated

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,9 @@
-
 const Dashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center w-screen md:max-w-screen-lg lg:max-w-screen-xl mx-auto">
+        
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/Pages/ClassDetails/ClassDetails.jsx
+++ src/Pages/ClassDetails/ClassDetails.jsx
@@ -1,23 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import Loading from "../../shared/Loading/Loading";
 import right from "../../assets/right.png";
 import { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./Payment/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useAuth from "../../hooks/useAuth";
 
 const stripePromise = loadStripe(import.meta.env.VITE_payment_gateway_pk);
 
 const ClassDetails = () => {
-  const { get } = useAxiosSecure();
+  const { get, post } = useAxiosSecure();
 
   const { id } = useParams();
-
+  const { user } = useAuth();
   const [selected, setSelected] = useState(1);
 
-  const { data: classDetails = [], isLoading } = useQuery({
+  const { data: paid = false } = useQuery({
+    queryKey: ["paid"],
+    queryFn: async () =>
+      await post("/verifyPayment", {
+        email: user.email,
+        classId: id,
+      }).then((res) => {
+        console.log(res.data);
+        return res.data.isPaid;
+      }),
+    refetchOnWindowFocus: false,
+  });
+
+  const { data = [], isLoading } = useQuery({
     queryKey: ["classDetails"],
     queryFn: async () =>
       await get(`/classDetails/${id}`).then((res) => res.data),
@@ -26,11 +40,14 @@ const ClassDetails = () => {
 
   if (isLoading) return <Loading />;
 
-  return (
-    <div className="w-full my-20 flex flex-col lg:flex-row items-start justify-center space-y-4 lg:space-y-0 lg:space-x-10">
+  return data.map((classDetails) => (
+    <div
+      key={classDetails._id}
+      className="w-full my-20 flex flex-col lg:flex-row items-start justify-center space-y-4 lg:space-y-0 lg:space-x-10"
+    >
       <div className="hero bg-base-200 w-full lg:w-[50%]">
         <div className="hero-content flex-col lg:flex-col">
-          <img src={classDetails[0].imageUrl} className="w-[96%]" />
+          <img src={classDetails.imageUrl} className="w-[96%]" />
           {/* tab section */}
           <div role="tablist" className="tabs tabs-border">
             <a
@@ -58,7 +75,7 @@ const ClassDetails = () => {
                     <span className="font-bold text-sm lg:text-lg">
                       Students:
                     </span>{" "}
-                    {classDetails[0].totalEnrollment}
+                    {classDetails.totalEnrollment}
                   </p>
                 </div>
                 <div className="flex items-center space-x-1">
@@ -67,15 +84,15 @@ const ClassDetails = () => {
                     <span className="font-bold text-sm lg:text-lg">
                       Instructor:
                     </span>{" "}
-                    {classDetails[0].name}
+                    {classDetails.name}
                   </p>
                 </div>
               </div>
               <h1 className="text-md lg:text-xl font-bold">
-                {classDetails[0].title}
+                {classDetails.title}
               </h1>
               <p className="text-md text-justify lg:text-lg py-6">
-                {classDetails[0].description}
+                {classDetails.description}
               </p>
             </div>
           ) : (
@@ -84,14 +101,14 @@ const ClassDetails = () => {
                 <figure>
                   <img
                     className="rounded-full w-[130px] h-[130px]"
-                    src={classDetails[0].photo}
-                    alt="Shoes"
+                    src={classDetails.photo}
+                    alt={`${classDetails.title}`}
                   />
                 </figure>
                 <div className="card-body lg:w-[70%]">
-                  <h2 className="card-title">{classDetails[0].name}</h2>
-                  <h2 className="card-title">{classDetails[0].designation}</h2>
-                  <p className="text-justify">{classDetails[0].experience}</p>
+                  <h2 className="card-title">{classDetails.name}</h2>
+                  <h2 className="card-title">{classDetails.designation}</h2>
+                  <p className="text-justify">{classDetails.experience}</p>
                 </div>
               </div>
             </div>
@@ -100,32 +117,44 @@ const ClassDetails = () => {
       </div>
       {/* pay section */}
       <div className="hero bg-base-200 w-full lg:w-[30%] flex flex-col items-start border-1 border-gray-300 p-4 space-y-4">
-        <div role="tablist" className="tabs tabs-border">
-          <a
-            role="tab"
-            className="tab tab-active text-2xl font-semibold cursor-default"
-          >
-            Price
-          </a>
-        </div>
-        <p className="text-2xl font-semibold">{classDetails[0].price} BDT</p>
-        <button
-          className="btn bg-[#FDC800] w-full text-lg"
-          onClick={() => document.getElementById("my_modal_5").showModal()}
-        >
-          Pay
-        </button>
+        {!paid ? (
+          <>
+            <div role="tablist" className="tabs tabs-border">
+              <a
+                role="tab"
+                className="tab tab-active text-2xl font-semibold cursor-default"
+              >
+                Price
+              </a>
+            </div>
+
+            <p className="text-2xl font-semibold">{classDetails.price} BDT</p>
+
+            <button
+              className="btn bg-[#FDC800] w-full text-lg"
+              onClick={() => document.getElementById("my_modal_5").showModal()}
+            >
+              Pay
+            </button>
+          </>
+        ) : (
+          <Link to="/" className="btn bg-[#FDC800] w-full text-lg">
+            Class Details
+          </Link>
+        )}
       </div>
       {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          <Elements stripe={stripePromise}>
-            <CheckoutForm id={id} price={classDetails[0].price}/>
-          </Elements>
-        </div>
-      </dialog>
+      {!paid && (
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box">
+            <Elements stripe={stripePromise}>
+              <CheckoutForm id={id} price={classDetails.price} />
+            </Elements>
+          </div>
+        </dialog>
+      )}
     </div>
-  );
+  ));
 };
 
 export default ClassDetails;
