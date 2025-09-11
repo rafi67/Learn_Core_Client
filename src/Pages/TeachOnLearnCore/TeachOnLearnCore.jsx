6bfd09a2f23fbe2f1a80f5e87bc03605c8e1ef8b
Rafi Siddique <rafisiddique67@gmail.com>
2025-09-11 18:11:31 +0600

    teach on learnCore page updated

--- src/Pages/TeachOnLearnCore/TeachOnLearnCore.jsx
+++ src/Pages/TeachOnLearnCore/TeachOnLearnCore.jsx
@@ -8,7 +8,7 @@ import useVerifyUser from "../../hooks/useVerifyUser";
 const TeachOnLearnCore = () => {
   const { user } = useAuth();
   const { post } = useAxiosSecure();
-  const {userType} = useVerifyUser();
+  const { userType } = useVerifyUser();
 
   const {
     register,
@@ -42,9 +42,9 @@ const TeachOnLearnCore = () => {
       experience: data.experience,
       title: data.title,
       category: data.category,
-      status: 'pending',
+      status: "pending",
     };
-    
+
     postMutation.mutate(teacherRequest);
   };
 
@@ -52,79 +52,86 @@ const TeachOnLearnCore = () => {
     <div className="hero bg-base-200 min-h-screen">
       <title>Teach on LearnCore</title>
       <div className="hero-content w-[50%] flex-col lg:flex-row-reverse">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <div className="avatar relative mx-auto">
-              <div className="w-24 rounded-full">
-                <img src={user.photoURL} />
+        {!(userType?.role == "teacher") && (
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <div className="avatar relative mx-auto">
+                <div className="w-24 rounded-full">
+                  <img src={user.photoURL} />
+                </div>
               </div>
+              <form onSubmit={handleSubmit(submit)} className="fieldset">
+                <legend className="fieldset-legend">Name</legend>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Name"
+                  value={user.displayName}
+                  readOnly
+                />
+                <legend className="fieldset-legend">Email</legend>
+                <input
+                  type="email"
+                  className="input"
+                  placeholder="email"
+                  value={user.email}
+                  readOnly
+                />
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Experience</legend>
+                  <select
+                    defaultValue="Select an Experience"
+                    className="select"
+                    {...register("experience", { required: true })}
+                  >
+                    <option disabled={true}>Select an Experience</option>
+                    <option>Beginner</option>
+                    <option>Experienced</option>
+                    <option>Mid-Level</option>
+                  </select>
+                  {errors.experience?.type === "required" && (
+                    <p className="text-lg text-red-500">experience required</p>
+                  )}
+                </fieldset>
+                <legend className="fieldset-legend">Title</legend>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Title"
+                  name="title"
+                  {...register("title", { required: true })}
+                />
+                {errors.title?.type === "required" && (
+                  <p className="text-lg text-red-500">title required</p>
+                )}
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Category</legend>
+                  <select
+                    defaultValue="Select a Category"
+                    className="select"
+                    {...register("category", { required: true })}
+                  >
+                    <option disabled={true}>Select a Category</option>
+                    <option>Software Engineer</option>
+                    <option>Database Engineer</option>
+                    <option>Mobile app Developer</option>
+                  </select>
+                  {errors.category?.type === "required" && (
+                    <p className="text-lg text-red-500">category required</p>
+                  )}
+                </fieldset>
+                <button
+                  className="btn btn-neutral mt-4"
+                  disabled={
+                    userType?.role === "student" || userType?.role === "admin"
+                  }
+                >
+                  Submit for Review
+                </button>
+              </form>
             </div>
-            <form onSubmit={handleSubmit(submit)} className="fieldset">
-              <legend className="fieldset-legend">Name</legend>
-              <input
-                type="text"
-                className="input"
-                placeholder="Name"
-                value={user.displayName}
-                readOnly
-              />
-              <legend className="fieldset-legend">Email</legend>
-              <input
-                type="email"
-                className="input"
-                placeholder="email"
-                value={user.email}
-                readOnly
-              />
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Experience</legend>
-                <select
-                  defaultValue="Select an Experience"
-                  className="select"
-                  {...register("experience", { required: true })}
-                >
-                  <option disabled={true}>Select an Experience</option>
-                  <option>Beginner</option>
-                  <option>Experienced</option>
-                  <option>Mid-Level</option>
-                </select>
-                {errors.experience?.type === "required" && (
-                  <p className="text-lg text-red-500">experience required</p>
-                )}
-              </fieldset>
-              <legend className="fieldset-legend">Title</legend>
-              <input
-                type="text"
-                className="input"
-                placeholder="Title"
-                name="title"
-                {...register("title", { required: true })}
-              />
-              {errors.title?.type === "required" && (
-                <p className="text-lg text-red-500">title required</p>
-              )}
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Category</legend>
-                <select
-                  defaultValue="Select a Category"
-                  className="select"
-                  {...register("category", { required: true })}
-                >
-                  <option disabled={true}>Select a Category</option>
-                  <option>Software Engineer</option>
-                  <option>Database Engineer</option>
-                  <option>Mobile app Developer</option>
-                </select>
-                {errors.category?.type === "required" && (
-                  <p className="text-lg text-red-500">category required</p>
-                )}
-              </fieldset>
-              <button className="btn btn-neutral mt-4" disabled={userType?.role==="student"||userType?.role==="admin"}>
-                Submit for Review
-              </button>
-            </form>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
