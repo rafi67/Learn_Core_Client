f7e04ba945cb7377b1e6f58d813fdf11b7c94edb
Rafi Siddique <rafisiddique67@gmail.com>
2025-09-15 09:24:52 +0600

    use pagination hook updated

--- src/hooks/usePagination.jsx
+++ src/hooks/usePagination.jsx
@@ -1,22 +1,20 @@
+import { useEffect, useState } from "react";
 import useAuth from "./useAuth";
-import { useQuery } from "@tanstack/react-query";
 
 const usePagination = (data) => {
   const { itemsPerPage, setNumberOfPages, currentPage, setCurrentPage } =
     useAuth();
 
-  const { data: paginatedData } = useQuery({
-    queryKey: ["pagination", itemsPerPage, currentPage, data],
-    queryFn: () => {
-      const total = Math.ceil(data.length / itemsPerPage);
+    const [paginatedData, setPaginatedData] = useState(data);
+
+    useEffect(() => {
+       const total = Math.ceil(data.length / itemsPerPage);
       if (total < currentPage) setCurrentPage(1);
       setNumberOfPages(total);
       const startIndex = (currentPage - 1) * itemsPerPage;
       const endIndex = startIndex + itemsPerPage;
-      return data.slice(startIndex, endIndex);
-    },
-    refetchOnWindowFocus: false,
-  });
+      setPaginatedData(data.slice(startIndex, endIndex));
+    }, [itemsPerPage, currentPage, data, setCurrentPage, setNumberOfPages]);
 
   return { paginatedData };
 };
