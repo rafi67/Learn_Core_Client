6ea3fcb1ebcdf7b766c78756beef5f80c7f66e09
Rafi Siddique <rafisiddique67@gmail.com>
2025-09-19 14:04:06 +0600

    feedback design issue fixed

--- src/Pages/Home/Feedback/Feedback.jsx
+++ src/Pages/Home/Feedback/Feedback.jsx
@@ -15,35 +15,41 @@ const Feedback = () => {
 
   return (
     <div className="w-screen xl:max-w-screen-lg mx-auto mt-16">
-      <h1 className="text-xl md:text-2xl lg:text-5xl font-bold text-center mb-4">Feedback</h1>
+      <h1 className="text-xl md:text-2xl lg:text-5xl font-bold text-center mb-4">
+        Feedback
+      </h1>
       <Swiper
         modules={[Pagination, Navigation, Autoplay]}
         navigation={true}
         pagination={{
           clickable: true,
         }}
-        autoplay={true}
+        // autoplay={true}
         style={{
           margin: "auto",
           "--swiper-navigation-color": "#FDC800",
           "--swiper-pagination-color": "#FDC800",
         }}
         slidesPerView={1}
-        loop={true}
+        // loop={true}
         className="mySwiper"
       >
         {feedback.map((f) => (
-          <SwiperSlide style={{height: "500px"}} key={f._id}>
-            <div className="hero bg-base-200 w-screen lg:max-w-[60%] h-[55%] m-auto relative top-30">
-              <div className="hero-content">
+          <SwiperSlide style={{ height: "500px" }} key={f._id}>
+            <div className="hero bg-base-200 w-screen items-center flex-col lg:max-w-[60%] h-[55%] m-auto relative top-30">
+              <div className="hero-content flex-col w-full">
                 <img
                   src={f.photoUrl}
-                  className="w-[200px] h-[200px] xl:max-w-sm rounded-full shadow-2xl border-8 border-white relative mx-auto left-15 lg:left-50 -top-30"
+                  className="w-[200px] h-[200px] xl:max-w-sm rounded-full shadow-2xl border-8 border-white relative mx-auto -top-30"
                 />
-                <div className="top-15 -left-21 relative text-center">
-                  <p className="text-[12px] md:text-lg lg:text-xl font-bold">{f.name}</p>
+                <div className="text-center -mt-28">
+                  <p className="text-[12px] md:text-lg lg:text-xl font-bold">
+                    {f.name}
+                  </p>
                   <h1 className="text-sm lg:text-lg font-bold">{f.title}</h1>
-                  <p className="text-sm lg:text-md line-clamp-2 lg:line-clamp-none text-justify">{f.feedbackText}</p>
+                  <p className="text-sm lg:text-md line-clamp-2 lg:line-clamp-none text-center">
+                    {f.feedbackText}
+                  </p>
                 </div>
               </div>
             </div>
